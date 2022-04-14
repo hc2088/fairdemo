8e4546fe8398db68e6dc5bd8780566b5e52dff79
huchu <huchu@58.com>
2022-04-14 17:16:23 +0800

    123

--- lib/simplepage.dart
+++ lib/simplepage.dart
@@ -2,8 +2,8 @@ import 'package:fair/fair.dart';
 import 'package:flutter/material.dart';
 
 @FairPatch()
-class simplepage extends StatelessWidget {
-  const simplepage({Key key}) : super(key: key);
+class SimplePage extends StatelessWidget {
+  const SimplePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
